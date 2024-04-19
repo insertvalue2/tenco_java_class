bd6fbe24f09560e0987e9fd30f912ac15573d428
kdt4 <insertvaue@naver.com>
2024-04-19 13:56:52 +0900

    static 메서드 및 버블 정렬

---
+++ src/basic/ch12/Card.java
@@ -0,0 +1,18 @@
+package basic.ch12;
+
+public class Card {
+	
+	// 멤버 변수,static 변수  
+	
+	// 생성자 
+	
+	
+	// 코드 테스트(메인 함수) 
+	public static void main(String[] args) {
+		
+	} // end of main
+	
+	// 멤버 함수 
+	// get,set 작성
+	
+} // end of class 
