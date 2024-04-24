6abbff84617f7184fb87f0b2450f0812c2d49417
kdt4 <insertvaue@naver.com>
2024-04-24 16:59:25 +0900

    추상클래스까지

---
+++ src/basic/exercise/toy/ToyMachine.java
@@ -0,0 +1,36 @@
+package basic.exercise.toy;
+
+// 클래스 - 인형뽑기
+// 상품들이 존재 가능 
+// 사자인형, 곰인형, 에어팟
+// 요구 조건 
+// 배열을 활용해서 객체들을 담아 주세요 
+public class ToyMachine {
+
+	Product[] products;
+
+	public ToyMachine() {
+		products = new Product[30];
+	}
+
+	// 사장이 상품을 넣다
+	public void addProduct(Product product) {
+
+	}
+
+	public void addProduct(Product[] products) {
+
+	}
+
+	// 사용자가 돈을 넣고 게임을 한다.
+	public void pick() {
+
+	}
+
+	// 상품에 정보 출력
+
+}
+
+class Product {
+
+}
---
+++ src/basic/exercise/toy/ToyMachineTest.java
@@ -0,0 +1,11 @@
+package basic.exercise.toy;
+
+public class ToyMachineTest {
+
+	// 코드 테스트 
+	public static void main(String[] args) {
+		
+
+	} // end of main 
+
+} // end of class 
